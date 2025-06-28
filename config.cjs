39a9174e05e1fdf66478b3f4df40f0544178bb62
ArslanMD Official <numberkliye@gmail.com>
2025-06-28 21:41:17 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "ARSLANMD~oysEXAjQ#Va3lGaapft1THmU3xwBH1cetOmvRAHNR6qgoM2rYRQA",
   PREFIX: process.env.PREFIX || '.',
   BOT_NAME: process.env.BOT_NAME || "<| Arslan-Ai-2.0 𝐕❷🫧",
   BOT: process.env.BOT || "hello 👋",
