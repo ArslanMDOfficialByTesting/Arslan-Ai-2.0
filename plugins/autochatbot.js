e75371e76d4ee2ae7e827abfec5413bdb95a61fa
ArslanMDOfficialByTesting <muhammadarslanmdofficial@gmail.com>
2025-06-29 11:56:49 +0500

    Update autochatbot.js

--- plugins/autochatbot.js
+++ plugins/autochatbot.js
@@ -1,7 +1,4 @@
-
-
-
-import config from '../../config.cjs';
+const config = require('../config.cjs');
 
 const autotypingCommand = async (m, Matrix) => {
   const botNumber = await Matrix.decodeJid(Matrix.user.id);
